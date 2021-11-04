a1ccc925a9543a8bba7410c001a61e7da81afb55
Oscar Vila <code_moscar@hotmail.com>
2021-11-03 23:10:14 -0600

    primer ejercicio de figueas

---
+++ figuras.js
@@ -0,0 +1,12 @@
+
+console.group("primer grupo - cuadrado-");
+//operaciones para cuadrado
+const ladoCuadrado=5;
+console.log("los lados del cuadrado miden "+ladoCuadrado);
+
+const perimetroCuadrado= ladoCuadrado*4;
+console.log("el perimetro del cuadrado es: "+perimetroCuadrado);
+
+const areaCuadrado= ladoCuadrado*ladoCuadrado;
+console.log("el area del cuadrado es: "+perimetroCuadrado);
+console.groupEnd();
